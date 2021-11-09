994f46716c4b294cd24ab7d27d75de3878fb2a94
Sean Collins <seancollins117@gmail.com>
2021-11-09 09:33:48 -0800

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -65,9 +65,17 @@ var fight = function(enemyName) {
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
+  if (playerHealth > 0) {
+    window.alert('Welcome to Robot Gladiators! Round ' + (i +1));
   var pickedEnemyName = enemyNames[i];
   enemyHealth = 50;
   fight(pickedEnemyName);
+  }
+
+  else {
+    window.alert('You have lost your robot in battle! Game Over!');
+    break;
+  }
 }
 
 fight();
